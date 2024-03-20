4d6bec4205e29924948cc4a3d91d7c92b5ca81b7
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-03-20 01:48:36 +0000

    comiited

--- colorChanger/script.js
+++ colorChanger/script.js
@@ -5,16 +5,16 @@ const buttons = document.querySelectorAll(".button")
 buttons.forEach(function(button){
     button.addEventListener('click',function(e){
         if(e.target.id == 'grey'){
-            body.style.backgroundColor = 'grey'
+            body.style.backgroundColor = e.target.id
         }
         if(e.target.id == 'white'){
-            body.style.backgroundColor = 'white'
+            body.style.backgroundColor = e.target.id
         }
         if(e.target.id == 'blue'){
-            body.style.backgroundColor = 'blue'
+            body.style.backgroundColor = e.target.id
         }
         if(e.target.id == 'yellow'){
-            body.style.backgroundColor = 'yellow'
+            body.style.backgroundColor = e.target.id
         }
 
     })
